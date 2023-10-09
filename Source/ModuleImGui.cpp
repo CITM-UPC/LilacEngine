7b0decbeffa581463dfc8f4c56e61374f4809148
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2023-10-09 18:08:12 +0200

    modal window added

--- Source/ModuleImGui.cpp
+++ Source/ModuleImGui.cpp
@@ -51,7 +51,7 @@ update_status ModuleImGui::Update(float dt)
 {
 	// HOMEWORK 1
 	ImGui::ShowDemoWindow();
-	ImGui::Begin("Homework 1");
+	ImGui::Begin("Homework 1", NULL);
 	if (ImGui::Button("Close the app"))
 		return UPDATE_STOP;
 	ImGui::End();
@@ -78,20 +78,41 @@ update_status ModuleImGui::Update(float dt)
 	}
 
 	if (about) {
-		ImGui::Begin("Help");
-		ImGui::Text("Lilac Engine");
-		ImGui::Text("A great engine in development");
-		ImGui::Text("by Júlia Serra Trujillo and Joel Chaves Moreno");
-		ImGui::Spacing();
-		ImGui::Text("3rd Party Libraries used:");
-		ImGui::Text("- SDL");
-		ImGui::Text("- ImGui");
-		ImGui::Spacing();
-		ImGui::Text("License:");
-		ImGui::Spacing();
-		ImGui::Text("MIT License");
-		ImGui::Spacing();
-		ImGui::Text("Copyright (c) 2023 Júlia Serra Trujillo and Joel Chaves Moreno");
+		ImGui::OpenPopup("About");
+		if (ImGui::BeginPopupModal("About", NULL, ImGuiWindowFlags_AlwaysAutoResize)) {
+			ImGui::Text("Lilac Engine");
+			ImGui::Text("An engine in development");
+			ImGui::Text("by Júlia Serra Trujillo and Joel Chaves Moreno\n\n");
+			ImGui::Text("3rd Party Libraries used:");
+			ImGui::Text("- SDL 2");
+			ImGui::Text("- ImGui\n\n");
+			ImGui::Text("License:\n\n");
+			ImGui::Text("MIT License\n\n");
+			// 1st Paragraph
+			ImGui::Text("Copyright (c) 2023 Júlia Serra Trujillo and Joel Chaves Moreno\n\n");
+			ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy\n");
+			ImGui::Text("of this software and associated documentation files(the Software), to deal\n");
+			ImGui::Text("in the Software without restriction, including without limitation the rights\n");
+			ImGui::Text("to use, copy, modify, merge, publish, distribute, sublicense, and /or sell\n");
+			ImGui::Text("copies of the Software, and to permit persons to whom the Software is\n");
+			ImGui::Text("furnished to do so, subject to the following conditions : \n\n");
+			// 2nd Paragraph
+			ImGui::Text("The above copyright notice and this permission notice shall be included in all\n");
+			ImGui::Text("copies or substantial portions of the Software.\n\n");
+			// 3rd
+			ImGui::Text("THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n");
+			ImGui::Text("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n");
+			ImGui::Text("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n");
+			ImGui::Text("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n");
+			ImGui::Text("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n");
+			ImGui::Text("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n");
+			ImGui::Text("SOFTWARE\n\n");
+
+			if (ImGui::Button("Close the window")) {
+				about = !about;
+			}
+			ImGui::EndPopup();
+		}
 	}
 
 #pragma endregion MENU
