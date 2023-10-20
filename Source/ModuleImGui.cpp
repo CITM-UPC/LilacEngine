db290b554fa9fbb459b15fb2d099cf04edc8de7d
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2023-10-20 12:47:44 +0200

    About corrected and added some functions in input

--- Source/ModuleImGui.cpp
+++ Source/ModuleImGui.cpp
@@ -52,7 +52,9 @@ update_status ModuleImGui::Update(float dt)
 {
 	ImGuiIO& io = ImGui::GetIO();
 #pragma region UI
-	
+	// For debugging
+	ImGui::ShowDemoWindow();
+
 	// Tabs
 	ImGui::BeginMainMenuBar();
 	if (ImGui::BeginMenu("General")) {
@@ -88,16 +90,16 @@ update_status ModuleImGui::Update(float dt)
 		if (ImGui::BeginPopupModal("About", NULL, ImGuiWindowFlags_AlwaysAutoResize)) {
 			ImGui::Text("Lilac Engine");
 			ImGui::Text("An engine in development");
-			ImGui::Text("by Júlia Serra Trujillo and Joel Chaves Moreno\n\n");
+			ImGui::Text(u8"by Júlia Serra Trujillo and Joel Chaves Moreno\n\n");
 			ImGui::Text("3rd Party Libraries used:");
 			ImGui::Text("- SDL 2");
 			ImGui::Text("- ImGui\n\n");
 			ImGui::Text("License:\n\n");
 			ImGui::Text("MIT License\n\n");
 			// 1st Paragraph
-			ImGui::Text("Copyright (c) 2023 Júlia Serra Trujillo and Joel Chaves Moreno\n\n");
+			ImGui::Text(u8"Copyright (c) 2023 Júlia Serra Trujillo and Joel Chaves Moreno\n\n");
 			ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy\n");
-			ImGui::Text("of this software and associated documentation files(the Software), to deal\n");
+			ImGui::Text("of this software and associated documentation files (\"the Software\"), to deal\n");
 			ImGui::Text("in the Software without restriction, including without limitation the rights\n");
 			ImGui::Text("to use, copy, modify, merge, publish, distribute, sublicense, and /or sell\n");
 			ImGui::Text("copies of the Software, and to permit persons to whom the Software is\n");
@@ -106,7 +108,7 @@ update_status ModuleImGui::Update(float dt)
 			ImGui::Text("The above copyright notice and this permission notice shall be included in all\n");
 			ImGui::Text("copies or substantial portions of the Software.\n\n");
 			// 3rd
-			ImGui::Text("THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n");
+			ImGui::Text("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n");
 			ImGui::Text("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n");
 			ImGui::Text("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n");
 			ImGui::Text("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n");
@@ -154,6 +156,8 @@ update_status ModuleImGui::Update(float dt)
 				ImGui::Text("Mouse Position: (%.1f,%.1f)", io.MousePos.x, io.MousePos.y);
 			else
 				ImGui::Text("Mouse Position: <invalid>");
+			ImGui::Text("Mouse delta: (%g, %g)", io.MouseDelta.x, io.MouseDelta.y);
+			ImGui::Text("Mouse wheel: %.1f", io.MouseWheel);
 		}
 		if (ImGui::CollapsingHeader("Audio")) {
 			ImGui::Text("No audio applied for now!", io.MousePos.x, io.MousePos.y);
