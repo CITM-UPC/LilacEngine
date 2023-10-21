66924da4608db2c1c6ec56661941d17e55e760ae
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2023-10-22 00:33:14 +0200

    Changed the functions in Mesh.h and Mesh.cpp

--- Source/Mesh.cpp
+++ Source/Mesh.cpp
@@ -1,11 +1,14 @@
 #include "Mesh.h"
 #include "GL/glew.h"
+#include "assimp/cimport.h" 
+#include "assimp/scene.h" 
+#include "assimp/postprocess.h"
 
 Mesh::Mesh(Formats format, const void* vertex_data, uint numVerts, const uint* index_data, uint numIndexs)
 {
 	// glGenBuffers(1, &vertex_buffer_id);
 	// glBinBuffer(_vertex_buffer_id);
-	switch (_formats) {
+	switch (_format) {
 	case Formats::F_V3T2:
 		glBufferData(GL_VERTEX_ARRAY, sizeof(double) * 5 * _numVerts, vertex_data, GL_STATIC_DRAW);
 	default:
@@ -19,19 +22,63 @@ Mesh::Mesh(Formats format, const void* vertex_data, uint numVerts, const uint* i
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
+
+Mesh::Mesh(Mesh&& b) noexcept :
+    _format(b._format),
+    _vertex_buffer_id(b._vertex_buffer_id),
+    _numVerts(b._numVerts),
+    _index_buffer_id(b._index_buffer_id),
+    _numIndexs(b._numIndexs),
+    texture(b.texture)
+{
+    b._vertex_buffer_id = 0;
+    b._index_buffer_id = 0;
+
+}
+
+Mesh::~Mesh() {
+    if (_vertex_buffer_id) glDeleteBuffers(1, &_vertex_buffer_id);
+    if (_index_buffer_id) glDeleteBuffers(1, &_index_buffer_id);
+}
+
 void Mesh::draw() {
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _vertex_buffer_id);
 
-	switch (_formats) {
-	case Formats::F_V3T2:
-		glEnable(GL_TEXTURE_2D);
-		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-		glVertexPointer(3, GL_DOUBLE, sizeof(double) * 5, nullptr);
-		glTexCoordPointer(2, GL_DOUBLE, sizeof(double) * 5, (void*)(sizeof(double) * 5));
-	default:
-		throw "not implemented";
-	}
+    glColor4ub(255, 255, 255, 255);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _index_buffer_id);
+    glBindBuffer(GL_ARRAY_BUFFER, _vertex_buffer_id);
+    glEnableClientState(GL_VERTEX_ARRAY);
+
+    switch (_format) {
+    case Formats::F_V3:
+        glVertexPointer(3, GL_FLOAT, 0, nullptr);
+        break;
+    case Formats::F_V3C4:
+        glEnableClientState(GL_COLOR_ARRAY);
+        glVertexPointer(3, GL_FLOAT, sizeof(V3C4), nullptr);
+        glColorPointer(4, GL_FLOAT, sizeof(V3C4), (void*)sizeof(V3));
+        break;
+    case Formats::F_V3T2:
+        glEnable(GL_TEXTURE_2D);
+        if (texture.get()) texture->bind();
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+        glVertexPointer(3, GL_FLOAT, sizeof(V3T2), nullptr);
+        glTexCoordPointer(2, GL_FLOAT, sizeof(V3T2), (void*)sizeof(V3));
+        break;
+    }
+
+    if (_index_buffer_id) {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _index_buffer_id);
+        glDrawElements(GL_TRIANGLES, _numIndexs, GL_UNSIGNED_INT, nullptr);
+    }
+    else {
+        glDrawArrays(GL_TRIANGLES, 0, _numVerts);
+    }
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisable(GL_TEXTURE_2D);
 }
--- Source/Mesh.h
+++ Source/Mesh.h
@@ -1,14 +1,21 @@
 #pragma once
 #include "Globals.h"
 #include "Texture.h"
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
 struct Mesh {
 	//friend struct Face;
 public:
 	enum class Formats { F_V3, F_V3T2, F_V3C4, F_V3CAT2 };
+	struct V3 { glm::dvec3 v; };
+	struct V3C4 { glm::dvec3 v; glm::dvec4 c; };
+	struct V3T2 { glm::dvec3 v; glm::dvec2 t; };
 
 private:
-	Formats _formats;
+	Formats _format;
 	
 	uint _index_buffer_id = 0; // index in VRAM 
 	uint _numIndexs = 0;
