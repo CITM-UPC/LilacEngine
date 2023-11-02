42ae426026ce359252f270b6ff7dff7ecfb027cb
Joel Chaves <99725590+JoeyCM@users.noreply.github.com>
2023-11-02 12:29:00 +0100

    camera stuff

--- Source/ModuleCamera3D.cpp
+++ Source/ModuleCamera3D.cpp
@@ -51,8 +51,10 @@ update_status ModuleCamera3D::Update(float dt)
 	if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
 		speed = 8.0f * dt;
 
-	if (App->input->GetKey(SDL_SCANCODE_R) == KEY_REPEAT) newPos.y += speed;
-	if (App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT) newPos.y -= speed;
+	if(App->input->GetKey(SDL_SCANCODE_T) == KEY_REPEAT) newPos.y += speed;
+	if(App->input->GetKey(SDL_SCANCODE_G) == KEY_REPEAT) newPos.y -= speed;
+	
+	if(App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT) newPos = (0, 0, 0);
 
 	if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos -= Z * speed;
 	if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos += Z * speed;
